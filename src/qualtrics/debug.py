ed7e46fedb78c7a44587b969510720892ebd9017
dheimgartner <d.heimgartners@gmail.com>
2022-11-10 15:00:59 +0100

    data dump added

---
+++ src/qualtrics/debug.py
@@ -0,0 +1,17 @@
+import pickle
+import tcsscraper.scrape as tcs
+
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(funcName)s %(message)s",
+    filename="logging.log",
+    level=logging.INFO,
+)
+
+with open("data/df_archs", "rb") as fp:
+    df_archs = pickle.load(fp)
+
+pc = df_archs.iloc[2]["car_objects"]
+
+car = tcs.get_cars(pc, 15e3, "ZH", similar={"flag": False}, headless=True, verbose=True)
